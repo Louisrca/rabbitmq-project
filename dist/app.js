444d4b6993985564390316d324b2407f5f58f766
Louis <roccalouis26@gmail.com>
2025-04-29 15:05:03 +0200

    add new producer

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,8 @@
-console.log("hello world!");
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const AddProducter_1 = __importDefault(require("./producer/AddProducter"));
+(0, AddProducter_1.default)();
 //# sourceMappingURL=app.js.map
--- src/producer/AddProducter.ts
+++ src/producer/AddProducter.ts
@@ -1,27 +1,30 @@
 import amqtplib from "amqplib";
 
+const rabbitmq_url = "amqp://user:password@localhost:5672";
+
+const clientNumber = {
+  n1: 1,
+  n2: 2,
+};
+
 async function AddProducer() {
   try {
-    const connection = await amqtplib.connect("amqp://localhost");
+    const connection = await amqtplib.connect(rabbitmq_url);
     const channel = await connection.createChannel();
     const queue = "additionnalOperationQueue";
 
     await channel.assertQueue(queue, { durable: true });
 
-    const product = {
-      id: 1,
-      name: "Product 1",
-      price: 100,
-    };
-
-    channel.sendToQueue(queue, Buffer.from(JSON.stringify(product)), {
-      persistent: true,
-    });
-    console.log("Product added to queue:", product);
-
-    setTimeout(() => {
-      connection.close();
-    }, 500);
+    setInterval(() => {
+      channel.sendToQueue(queue, Buffer.from(JSON.stringify(clientNumber)), {
+        persistent: true,
+      });
+      console.log(
+        `Producer is running and sending number to the queue: ${JSON.stringify(
+          clientNumber
+        )}`
+      );
+    }, 5000);
   } catch (error) {
     console.error("Error connecting to RabbitMQ:", error);
   }
